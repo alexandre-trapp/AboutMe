00f19f70b53489051c02e26234c8ab0c70d430e2
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-06-01 17:35:28 -0300

    add nickname

--- app/src/main/java/com/trapp/aboutme/MainActivity.kt
+++ app/src/main/java/com/trapp/aboutme/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.trapp.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        hideKeyboard(view)
+    }
+
+    private fun hideKeyboard(view: View) {
+
+        val input = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        input.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
