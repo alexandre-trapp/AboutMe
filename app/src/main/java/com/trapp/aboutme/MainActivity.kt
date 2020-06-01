59a8cdc0548f8404c8567a91f63c28a83a6f829a
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-06-01 18:13:18 -0300

    using data bindings

--- app/src/main/java/com/trapp/aboutme/MainActivity.kt
+++ app/src/main/java/com/trapp/aboutme/MainActivity.kt
@@ -1,35 +1,39 @@
 package com.trapp.aboutme
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.trapp.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.doneButton.setOnClickListener {
             addNickName(it)
         }
     }
 
     private fun addNickName(view: View) {
 
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         hideKeyboard(view)
     }
